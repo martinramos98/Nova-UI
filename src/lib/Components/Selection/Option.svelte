3c09c4faad6595f2c61d5ed73c465e2cbb2487ac
Martin Ramos <martinarielramos329@gmail.com>
2024-03-28 11:22:14 -0300

    wip selection

---
+++ src/lib/Components/Selection/Option.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let value
+  export let labelText = ''
+
+</script>
+<div>
+<slot>
+  {}
+</slot>
+</div>
---
+++ src/lib/Components/Selection/Selection.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+  export let multiselection = false
+	export let selectionLabel = '';
+  export let classNameLabel = ''
+  export let classNameOptionsContainer=''
+  const selected = writable([]);
+	setContext('ui-selection', {
+		selected
+	});
+
+</script>
+
+<div class="ui-selection">
+	<label class="ui-selection-label">
+		{selectionLabel}
+	</label>
+	<div class="ui-selection-options-container">
+    <slot/>
+  </div>
+</div>
