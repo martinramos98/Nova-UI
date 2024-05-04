8854b9e43634e13f722e45a35f402fe2f962dac0
Martin <martinarielramos329@gmail.com>
2024-05-04 15:46:35 -0300

    fix: change layer on top of html

--- src/lib/providers/NovaUIProvider.svelte
+++ src/lib/providers/NovaUIProvider.svelte
@@ -1,13 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
 	import '../main.css';
-	onMount(() => {
-		const style = document.createElement('style');
-		style.innerHTML = `
-		@layer theme, base, nova, components, utilities;
-		`;
-		document.head.prepend(style);
-	});
+	// onMount(() => {
+	// 	const style = document.createElement('style');
+	// 	style.innerHTML = `
+	// 	@layer theme, base, nova, components, utilities;
+	// 	`;
+	// 	document.head.prepend(style);
+	// });
 </script>
 
 <slot />
