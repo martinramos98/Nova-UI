23bfd6ef7f5e5773c6c84b3bde23793356afa022
Martin <martinarielramos329@gmail.com>
2024-05-04 15:37:22 -0300

    fix: on mount sets layer

--- src/lib/providers/NovaUIProvider.svelte
+++ src/lib/providers/NovaUIProvider.svelte
@@ -1,10 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../main.css';
+	onMount(() => {
+		const style = document.createElement('style');
+		style.innerHTML = `
+		@layer theme, base, nova, components, utilities;
+		`;
+		document.head.prepend(style);
+	});
 </script>
 
-<svelte:head>
-	<style type="text/css">
-		@layer theme, base, nova, components, utilities;
-	</style>
-</svelte:head>
 <slot />
