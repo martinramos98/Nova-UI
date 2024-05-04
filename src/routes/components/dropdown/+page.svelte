9a0b739e5761f412388dd8ebe94fc9b02515f3c7
Martin <martinarielramos329@gmail.com>
2024-05-04 15:05:09 -0300

    fix dropdowngroup imports path and name

--- src/routes/components/dropdown/+page.svelte
+++ src/routes/components/dropdown/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import Divider from '$lib/Components/Divider/Divider.svelte';
+	import DropdownGroup from '$lib/Components/Dropdown/DropdownGroup/DropdownGroup.svelte';
 	import DropDownItem from '$lib/Components/Dropdown/DropDownItem/DropDownItem.svelte';
 	import Dropdown from '$lib/Components/Dropdown/Dropdown.svelte';
-	import DropDownGroup from '$lib/Components/Dropdown/DropdownGroup/DropdownGroup.svelte';
 	import Title from '$lib/Components/Title/Title.svelte';
 </script>
 
@@ -86,7 +86,7 @@
 					<Dropdown triggerText="Open Dropdown">
 						<p class="font-bold pl-2 my-2">Menues</p>
 						<Divider size={1}></Divider>
-						<DropDownGroup>
+						<DropdownGroup>
 							{#snippet triggerContent()}
 								<span>Menu 1</span>
 							{/snippet}
@@ -94,8 +94,8 @@
 							<DropDownItem className="px-2">Item 1</DropDownItem>
 							<DropDownItem className="px-2">Item 2</DropDownItem>
 							<DropDownItem className="px-2">Item 3</DropDownItem>
-						</DropDownGroup>
-						<DropDownGroup>
+						</DropdownGroup>
+						<DropdownGroup>
 							{#snippet triggerContent()}
 								<span>Menu 2</span>
 							{/snippet}
@@ -105,28 +105,28 @@
 							<DropDownItem className="px-2">Item 3</DropDownItem>
 							<DropDownItem className="px-2">Item 4</DropDownItem>
 							<DropDownItem className="px-2">Item 5</DropDownItem>
-						</DropDownGroup>
-						<DropDownGroup>
+						</DropdownGroup>
+						<DropdownGroup>
 							{#snippet triggerContent()}
 								<span>Menu 3</span>
 							{/snippet}
 							<p class="font-bold my-2 italic">Menu 3</p>
 							<DropDownItem className="px-2">Item 1</DropDownItem>
 							<DropDownItem className="px-2">Item 2</DropDownItem>
-						</DropDownGroup>
-						<DropDownGroup position="right" classNameContainer="w-[200px] shadow-md">
+						</DropdownGroup>
+						<DropdownGroup position="right" classNameContainer="w-[200px] shadow-md">
 							{#snippet triggerContent()}
 								<span>Open subsection</span>
 							{/snippet}
 							<p class="font-bold text-center my-2">Subsection 1</p>
-							<DropDownGroup>
+							<DropdownGroup>
 								{#snippet triggerContent()}
 									<span>Open subsection</span>
 								{/snippet}
 								<p class="font-bold my-2 italic">Subsub section</p>
 								<DropDownItem>Item Subsub section</DropDownItem>
-							</DropDownGroup>
-						</DropDownGroup>
+							</DropdownGroup>
+						</DropdownGroup>
 					</Dropdown>
 				</div>
 			</section>
