6b2b988ba6fa0db4c7c97f0200b8b742d15e8d37
Martin <martinarielramos329@gmail.com>
2024-10-02 19:45:59 -0300

    added type for icons and json static props

--- packages/components/Icons/src/lib/Icon.svelte
+++ packages/components/Icons/src/lib/Icon.svelte
@@ -1,9 +1,10 @@
 <svelte:options runes={true} />
 
 <script lang="ts">
+	import { type SVGAttributes } from 'svelte/elements';
 	import type { Snippet } from 'svelte';
 
-	const { props, children }: { props: any; children: Snippet } = $props();
+	const { props, children }: { props: SVGAttributes<SVGElement>; children: Snippet } = $props();
 </script>
 
 <svg {...props}>
