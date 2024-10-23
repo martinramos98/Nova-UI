43ecb51c5b1f92a4ee6c2dbce53576b8f7b9c75c
Martin <martinarielramos329@gmail.com>
2024-10-22 21:31:09 -0300

    added types

---
+++ packages/components/index.d.ts
@@ -0,0 +1,80 @@
+import { Accordion, AccordionSection } from '@nv-org/accordion';
+import { Alert } from '@nv-org/alert';
+import { Avatar, AvatarGroup } from '@nv-org/avatar';
+import { Badge } from '@nv-org/badge';
+// import { Breadcrumb } from "@nv-org/breadcrumb";
+import { Button, ButtonGroup } from '@nv-org/button';
+import { Callout } from '@nv-org/callout';
+import { Card } from '@nv-org/card';
+import { Chip } from '@nv-org/chip';
+import { CollapsableBlock } from '@nv-org/collapsable-block';
+import { ContextMenu } from '@nv-org/context-menu';
+import { Divider } from '@nv-org/divider';
+import { Drawer } from '@nv-org/drawer';
+import { DropdownGroup, Dropdown, DropdownItem } from '@nv-org/dropdown';
+import { Icon } from '@nv-org/icon';
+import { Image } from '@nv-org/image';
+import { Input } from '@nv-org/input';
+import { Link } from '@nv-org/link';
+import { Loader } from '@nv-org/loader';
+import { Modal } from '@nv-org/modal';
+import { Navbar } from '@nv-org/navbar';
+import { Popover } from '@nv-org/popover';
+import { Progress } from '@nv-org/progress';
+import { Radio, RadioButton, RadioGroup } from '@nv-org/radio';
+import { Selection } from '@nv-org/selection';
+import { Slider } from '@nv-org/slider';
+import { Switch } from '@nv-org/switch';
+import { Table, TableBody, TableFooter, TableHeader, TableRow, Cell } from '@nv-org/table';
+import { Tabs, Tab, TabButton } from '@nv-org/tabs';
+import { Title } from '@nv-org/title';
+import { toast } from '@nv-org/toast';
+import { Tooltip } from '@nv-org/tooltip';
+export {
+	Accordion,
+	AccordionSection,
+	Alert,
+	Avatar,
+	AvatarGroup,
+	Badge,
+	// Breadcrumb,
+	Button,
+	ButtonGroup,
+	Callout,
+	Card,
+	Chip,
+	CollapsableBlock,
+	ContextMenu,
+	Divider,
+	Drawer,
+	DropdownGroup,
+	Dropdown,
+	DropdownItem,
+	Icon,
+	Image,
+	Input,
+	Link,
+	Loader,
+	Modal,
+	Navbar,
+	Popover,
+	Progress,
+	Radio,
+	RadioButton,
+	RadioGroup,
+	Selection,
+	Slider,
+	Switch,
+	Table,
+	TableBody,
+	TableFooter,
+	TableHeader,
+	TableRow,
+	Cell,
+	Tabs,
+	Tab,
+	TabButton,
+	Title,
+	toast,
+	Tooltip
+};
