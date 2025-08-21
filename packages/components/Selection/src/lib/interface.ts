4786bcdec629cc0012f5574c7875ee0288973511
Martin Ramos <martinarielramos329@gmail.com>
2025-08-21 20:24:24 -0300

    feature: now selection supports custom labels, changed animation box upen to svelte animation lib and added some utils

---
+++ packages/components/Selection/src/lib/interface.ts
@@ -0,0 +1,49 @@
+import type { Snippet } from 'svelte';
+import type { HTMLLabelAttributes } from 'svelte/elements';
+
+export interface ISelectionProps {
+	multiselection?: boolean;
+	type?: SelectionTypes;
+	variant?: SelectionVariants;
+	colors?: string;
+	selectionLabel?: string;
+	disabled?: boolean;
+	isInvalid?: boolean;
+	errorMessage?: string;
+	label?: {
+		labelPosition?: 'inside' | 'outside';
+	} & HTMLLabelAttributes;
+	class?: string;
+	classOptionsContainer?: string;
+	classSelected?: string;
+	classInputBox?: string;
+	placeholder?: string;
+	selectedValue?: Set<any>;
+	open?: boolean;
+	onSelect?: undefined | ((value: any) => void);
+	children: Snippet;
+	transitionSelectionBoxProps?: {
+		easing: any;
+		duration: number;
+	};
+}
+
+export type SelectionVariants =
+	| 'default'
+	| 'primary'
+	| 'secondary'
+	| 'tertiary'
+	| 'success'
+	| 'warning'
+	| 'danger'
+	| string;
+
+export type SelectionTypes =
+	| 'default'
+	| 'outlined'
+	| 'filled'
+	| 'underlined'
+	| 'blurred'
+	| 'flat'
+	| 'faded'
+	| string;
